780c8eeeb7aa0fc5d91f746ec90a4bb1b05d5f08
Elizabeth Martin <mothereffingsufferage@gmail.com>
2021-11-18 08:25:17 -0500

    this build does not actually work but i can't be fucked to fix it at the moment

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from './pixi.min.js';
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+// Create a Pixi Application
+const app = new PIXI.Application({});
+
+// Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+app.resizeTo = window;
